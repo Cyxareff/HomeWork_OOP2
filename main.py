1a082266fdd9c61f40258e2845484bca899e66f1
Cyxareff <suhov_j2001@rambler.ru>
2021-08-14 13:13:17 +0300

    rate_lecturer

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grades):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress
+            lecturer.grades += [grades]
+        else:
+            return 'Ошибка'
+
+
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -15,7 +23,16 @@ class Mentor:
         self.courses_attached = []
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = []
